1ff94d36fe34ee2eb2cb7c4c63f30e1ffcc18aee
iva-a-a <ivanova.alena.a98@gmail.com>
2025-11-27 19:09:08 +0200

    добавлен DeviceRepository

---
+++ Scanner/Scanner/Data/Repository/Device/DeviceRepositoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceRepositoryProtocol.swift
+//  Scanner
+//
+//  Created by Alena Ivanova on 27.11.2025.
+//
+
+import Foundation
+
+protocol DeviceRepositoryProtocol {
+    func saveDevices(_ devices: [Device], to sessionId: UUID) async throws
+    func fetchDevices(for sessionId: UUID) async throws -> [Device]
+    func deleteDevices(for sessionId: UUID) async throws
+    func deleteAllDevices() async throws
+}
